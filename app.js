89c2ba27e4b1e040ab9b6cf97e2d7438b32bbf6a
Antoine <65955606+Antoine-Flo@users.noreply.github.com>
2023-01-25 14:07:52 +0100

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+// make a function that can give me a report for a user
+
+// userReport('Sam') 
+const ex = {
+  name: 'Same',
+  age: 25,
+  ownedGames: ['League of Legends', 'Chess'],
+  reviews: [
+    {
+      title: 'Clue',
+      comment: 'A bit too complex for my taste', 
+      rating: 6
+    },
+    {
+      title: 'Minecraft',
+      comment: "It's fun but a bit repetitive", 
+      rating: 7
+    }
+  ],
+  bestGamePlayed: {
+    title: 'Monopoly',
+    rating: 9
+  }
+}
---
+++ datas.js
@@ -0,0 +1,86 @@
+const games = [
+  {
+    title: 'Monopoly',
+    rating: 9,
+    owner: 'John',
+    players: [
+      { name: 'John', age: 30 },
+      { name: 'Samantha', age: 25 },
+      { name: 'Ben', age: 22 },
+    ],
+    reviews: [
+      { user: 'Alex', comment: 'Great game, lots of fun!', rating: 8 },
+      { user: 'Emily', comment: 'I love the strategy aspect of this game', rating: 9 },
+    ],
+  },
+  {
+    title: 'Chess',
+    rating: 8,
+    owner: 'Samantha',
+    players: [
+      { name: 'Samantha', age: 25 },
+      { name: 'Ben', age: 22 },
+      { name: 'Emily', age: 28 },
+    ],
+    reviews: [
+      { user: 'John', comment: 'Challenging and fun', rating: 7 },
+      { user: 'Alex', comment: 'A classic game that never gets old', rating: 8 },
+    ],
+  },
+  {
+    title: 'Clue',
+    rating: 7,
+    owner: 'Ben',
+    players: [
+      { name: 'Ben', age: 22 },
+      { name: 'Emily', age: 28 },
+      { name: 'Alex', age: 27 },
+    ],
+    reviews: [
+      { user: 'John', comment: 'I love the mystery aspect of this game', rating: 8 },
+      { user: 'Samantha', comment: 'A bit too complex for my taste', rating: 6 },
+    ],
+  },
+    {
+    title: 'Fortnite',
+    rating: 8.5,
+    owner: 'John',
+    players: [
+      { name: 'John', age: 30 },
+      { name: 'Samantha', age: 25 },
+      { name: 'Ben', age: 22 },
+    ],
+    reviews: [
+      { user: 'Alex', comment: 'Great game, but too much time consuming', rating: 8 },
+      { user: 'Emily', comment: 'I love the graphics and the battle royale mode', rating: 9 },
+    ],
+  },
+  {
+    title: 'League of Legends',
+    rating: 8,
+    owner: 'Samantha',
+    players: [
+      { name: 'Samantha', age: 25 },
+      { name: 'Ben', age: 22 },
+      { name: 'Emily', age: 18 },
+    ],
+    reviews: [
+      { user: 'John', comment: 'Challenging and fun', rating: 8 },
+      { user: 'Alex', comment: 'A classic MOBA game that never gets old', rating: 9 },
+    ],
+  },
+  {
+    title: 'Minecraft',
+    rating: 7.5,
+    owner: 'Ben',
+    players: [
+      { name: 'Ben', age: 22 },
+      { name: 'Emily', age: 18 },
+      { name: 'Alex', age: 20 },
+    ],
+    reviews: [
+      { user: 'John', comment: 'I love the creative aspect of this game', rating: 8 },
+      { user: 'Samantha', comment: "It's fun but a bit repetitive", rating: 7 },
+    ],
+  },
+];
